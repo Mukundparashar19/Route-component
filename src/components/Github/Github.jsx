e8a28934ea2dfa63882bf7c02f9e66f49fca087f
Mukund Parashar <mukundparashar004@gmail.com>
2024-09-02 15:53:49 +0530

    Added:folders

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react'
+
+function Github() {
+    const [data, setData] = useState([])
+useEffect(()=> {
+    fetch('https://api.github.com/users/hiteshchoudhary')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        setData(data)
+        })
+
+},[])
+
+  return (
+    <div className='text-center m-4 bg-blue-600 text-white p-4 text-3xl'>
+      GITHUN:
+    </div>
+  )
+}
+
+export default Github
